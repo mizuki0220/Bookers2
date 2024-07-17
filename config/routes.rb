10960c55e26af833d000c4ca9c2bce4bff6fb0a7
mizuki0220 <em9220me.i@gmail.com>
2024-07-17 12:35:25 +0000

    [interruption]4-29

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do 
+  resources :books, only: [:new, :create, :index, :show, :destroy] do 
     resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end 
   
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
