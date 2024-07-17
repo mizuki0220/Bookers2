9f0b867a1e691cdc384db24ee40d1ebad8a89062
mizuki0220 <em9220me.i@gmail.com>
2024-07-17 14:53:44 +0000

    [finish]4-34

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+  get 'homes/about' => 'homes#about', as: 'about'
   
   resources :books, only: [:new, :create, :index, :show, :destroy] do 
     resource :favorite, only: [:create, :destroy]
