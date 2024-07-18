581e7fdc1b8d675390767df8db2e158357bf9975
mizuki0220 <em9220me.i@gmail.com>
2024-07-18 15:13:33 +0000

    [finish]4-34

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,24 @@ class BooksController < ApplicationController
   end
 
   def index
+    @user = current_user
+    @book = Book.new
   end
 
   def show
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    render_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
