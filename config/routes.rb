5950d0a93243653a2617565a3ce10eb826bbb9a9
mizuki0220 <em9220me.i@gmail.com>
2024-08-28 16:07:30 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
 
   resources :users, only: [:create, :index, :show, :edit, :update]
-
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #devise_for :users, controllers: { registrations: 'registrations' }
 end
